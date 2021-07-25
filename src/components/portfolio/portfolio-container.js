8f5dc1745de9f4550fa11e746fe228845225d694
David Briggs <dhbriggs@yahoo.com>
2021-07-25 09:03:48 -0400

    updated

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import PortfolioItem  from "./portfolio-item";
 
@@ -18,6 +19,8 @@ export default class PortfolioContainer extends Component {
        };
        
        this.handleFilter = this.handleFilter.bind(this);
+       this.getPortfolioItems = this.getPortfolioItems.bind(this);
+
     }
 
     handleFilter(filter) {
@@ -27,6 +30,17 @@ export default class PortfolioContainer extends Component {
             }) 
         })
     }
+
+    getPortfolioItems() {
+        axios
+        .get('https://davidbriggs.devcamp.space/portfolio/portfolio_items')
+        .then(response => {
+          console.log("response data", response);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      }
     
     portfolioItems() {
         return this.state.data.map(item => {
@@ -40,6 +54,9 @@ export default class PortfolioContainer extends Component {
         if (this.state.isLoading) {
            return <div>Loading...</div>;
         }
+
+        this.getPortfolioItems();
+
         return (
             <div>
                 <h2>{this.state.pageTitle}</h2>
